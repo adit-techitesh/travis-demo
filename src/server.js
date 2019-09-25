1e0b98421f691fd0354a662c44b3e10b9526a7f0
hitesh.n <hiteshnagda@adit.com>
2019-09-25 10:55:32 +0530

    Tetsing lint

--- src/server.js
+++ src/server.js
@@ -3,11 +3,11 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
-    res.send("Hello World!");
+    res.send("Hello World!")
 });
 
 app.listen(port, () => {
-    console.log(`Example app listening on port port ${port}!`);
+    console.log(`Example app listening on port port ${port}!`)
 });
 
 //Run app, then load http://localhost:port in a browser to see the output.
